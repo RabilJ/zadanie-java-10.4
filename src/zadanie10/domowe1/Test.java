f7f421b370fc73c5721fa872678f26218cb38aaf
RabilJ <jakub.popielarski7@gmail.com>
2018-10-15 17:10:21 +0200

    Zadanie 10.1 poprawka2

--- src/zadanie10/domowe1/Test.java
+++ src/zadanie10/domowe1/Test.java
@@ -2,7 +2,14 @@ package zadanie10.domowe1;
 
 public class Test {
     public static void main(String[] args) {
-        Person person1 = PersonCreator.birth();
-        System.out.println(person1);
+        try {
+            Person person1 = PersonCreator.birth();
+            System.out.println(person1);
+        }catch(IncorrectAgeException e){
+            System.err.print("Wiek nie może być ujemny");
+        }catch(NameUndefinedException ex){
+            System.err.print("Imię nie może się składać z 2(lub mniej) znaków");
+        }
+
     }
 }
